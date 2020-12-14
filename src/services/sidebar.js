f0141c359718b18aed789209cc6d861497b57320
Luis Valgoi <luis.valgoi@sap.com>
2020-12-14 20:38:58 -0300

    feat: added subs service

--- src/services/sidebar.js
+++ src/services/sidebar.js
@@ -44,10 +44,56 @@ const SIDEBAR_ITEMS_GROUP_2 = [
   },
 ];
 
+const SIDEBAR_SUBSCRIPTIONS = [
+  {
+    id: "flowpodcast",
+    text: "Flow Podcast",
+    avatar: "https://yt3.ggpht.com/ytc/AAUvwnh3qcXoKDGfaEE_tqvBu-VCHcUg0lEPXjBNFT4rgA=s88-c-k-c0x00ffffff-no-rj",
+    live: true,
+    news: false,
+  },
+  {
+    id: "rocketseat",
+    text: "Rocketseat",
+    avatar: "https://yt3.ggpht.com/ytc/AAUvwniZFvNe5OEM2bTjcGRdH8fOW06pr9HYSJCwDPK_BA=s88-c-k-c0x00ffffff-no-rj",
+    live: true,
+    news: false,
+  },
+  {
+    id: "stormer",
+    text: "Stormer",
+    avatar: "https://yt3.ggpht.com/ytc/AAUvwngA5eFNDlG5i-9yopoC240MX6mSeBlkfinP4QRY=s88-c-k-c0x00ffffff-no-rj",
+    live: false,
+    news: true,
+  },
+  {
+    id: "matheuscastiglioni",
+    text: "Matheus Castigliolini",
+    avatar: "https://yt3.ggpht.com/ytc/AAUvwnhUFkOaD4EVK2acel-KIZ0REYDdxS-DbK6YZRJvsg=s88-c-k-c0x00ffffff-no-rj",
+    live: false,
+    news: true,
+  },
+  {
+    id: "marquesbrownlee",
+    text: "Marques Brownlee",
+    avatar: "https://yt3.ggpht.com/ytc/AAUvwnhoa6jtz7ccGj30Al2Ha8t4ImDVW2U98STxoLXp=s88-c-k-c0x00ffffff-no-rj",
+    live: false,
+    news: true,
+  },
+];
+
 export function getSidebarItems() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve([SIDEBAR_ITEMS_GROUP_1, SIDEBAR_ITEMS_GROUP_2]);
     }, 1000);
+  });
+}
+
+export function getSubscriptions() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(SIDEBAR_SUBSCRIPTIONS);
+    }, 1500);
   });
 }
