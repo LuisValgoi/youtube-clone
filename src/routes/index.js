6ccf897f4c021b7d94f0b02acbaa20fe1f7a06a9
Valgoi <luis.valgoi@sap.com>
2020-12-18 21:45:44 -0300

    fix: routin bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,31 +17,35 @@ import Premium from "../pages/Premium";
 const Routes = () => {
   const { signed } = useAuth();
 
-  return (
-    <Switch>
-      <Route key={RouteConstants.TRENDING.key} path={RouteConstants.TRENDING.path} component={Trending} exact />
-      <Route key={RouteConstants.PREMIUM.key} path={RouteConstants.PREMIUM.path} component={Premium} exact />
+  const getRoutesForSignedUsers = () => {
+    return (
+      <Switch>
+        <Route key={RouteConstants.HOME.key} path={RouteConstants.HOME.path} component={Home} exact />
+        <Route key={RouteConstants.TRENDING.key} path={RouteConstants.TRENDING.path} component={Trending} exact />
+        <Route key={RouteConstants.PREMIUM.key} path={RouteConstants.PREMIUM.path} component={Premium} exact />
+        <Route key={RouteConstants.SUBSCRIPTIONS.key} path={RouteConstants.SUBSCRIPTIONS.path} component={Subscription} exact />
+        <Route key={RouteConstants.LIBRARY.key} path={RouteConstants.LIBRARY.path} component={Library} exact />
+        <Route key={RouteConstants.HISTORY.key} path={RouteConstants.HISTORY.path} component={History} exact />
+        <Route key={RouteConstants.YOUR_VIDEOS.key} path={RouteConstants.YOUR_VIDEOS.path} component={YourVideos} exact />
+        <Redirect from={RouteConstants.LOGIN.path} to={RouteConstants.HOME.path} />
+        <Route exact path="*" component={NotFound} />
+      </Switch>
+    );
+  };
 
-      {signed && (
-        <>
-          <Redirect from={RouteConstants.LOGIN.path} to={RouteConstants.HOME.path} />
-          <Route key={RouteConstants.HOME.key} path={RouteConstants.HOME.path} component={Home} exact />
-          <Route key={RouteConstants.SUBSCRIPTIONS.key} path={RouteConstants.SUBSCRIPTIONS.path} component={Subscription} exact />
-          <Route key={RouteConstants.LIBRARY.key} path={RouteConstants.LIBRARY.path} component={Library} exact />
-          <Route key={RouteConstants.HISTORY.key} path={RouteConstants.HISTORY.path} component={History} exact />
-          <Route key={RouteConstants.YOUR_VIDEOS.key} path={RouteConstants.YOUR_VIDEOS.path} component={YourVideos} exact />
-        </>
-      )}
+  const getRoutesForUnauthorizedUsers = () => {
+    return (
+      <Switch>
+        <Route key={RouteConstants.LOGIN.key} path={RouteConstants.LOGIN.path} component={Login} exact />
+        <Route key={RouteConstants.TRENDING.key} path={RouteConstants.TRENDING.path} component={Trending} exact />
+        <Route key={RouteConstants.PREMIUM.key} path={RouteConstants.PREMIUM.path} component={Premium} exact />
+        <Redirect from={RouteConstants.HOME.path} to={RouteConstants.LOGIN.path} />
+        <Route exact path="*" component={NotFound} />
+      </Switch>
+    );
+  };
 
-      {!signed && (
-        <>
-          <Redirect from="*" to={RouteConstants.LOGIN.path} />
-          <Route key={RouteConstants.LOGIN.key} path={RouteConstants.LOGIN.path} component={Login} exact />
-        </>
-      )}
-      <Route path="*" component={NotFound} />
-    </Switch>
-  );
+  return <>{signed ? getRoutesForSignedUsers() : getRoutesForUnauthorizedUsers()}</>;
 };
 
 export default Routes;
